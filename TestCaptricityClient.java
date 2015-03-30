367c1f19a80b688d19c80b09ef7e27561c715ba0
Dave <daves@captricity.com>
2015-03-30 09:23:25 -0400

    used full range of methods in CaptricityClient class in test client

--- TestCaptricityClient.java
+++ TestCaptricityClient.java
@@ -6,17 +6,20 @@ public class TestCaptricityClient {
   public static void main(String[] args) {
     try {
 			String apiToken = System.getenv("TEST_API_TOKEN");
+			String testFileLocation = "/Users/davids/Desktop/EZ-return1.pdf";
+			String testBatchName = "Test Java Batch-1";
 			
 			CaptricityClient capClient = new CaptricityClient(apiToken);
 			
-			// JSONObject newBatch = createBatch(apiToken, "Test Java Batch-1", true, false);
-			// System.out.println("New Batch:  " + newBatch.getInt("id") + ", " + newBatch.getString("name"));
-			// System.out.println();
-			
-			// JSONObject batchFile = addFileToBatch(apiToken, newBatch.getInt("id"), "/Users/davids/Desktop/EZ-return1.pdf");
-			// System.out.println("Batch File:  " + batchFile.getString("file_name") + ", " + batchFile.getString("uuid"));
-			// // System.out.println(batchFile);
-			// System.out.println();
+			JSONObject newBatch = capClient.createBatch(testBatchName, true, false);
+			System.out.println("New Batch:  " + newBatch.getInt("id") + ", " + newBatch.getString("name"));
+			// System.out.println(newBatch);
+			System.out.println();
+
+			JSONObject batchFile = capClient.addFileToBatch(newBatch.getInt("id"), testFileLocation);
+			System.out.println("Batch File:  " + batchFile.getString("file_name") + ", " + batchFile.getString("uuid"));
+			// System.out.println(batchFile);
+			System.out.println();
 			
       JSONArray batches = capClient.showBatches();
 			int numOfBatches = batches.length();
@@ -32,18 +35,18 @@ public class TestCaptricityClient {
 			}
 			System.out.println();
 			
-			// JSONArray documents = showDocuments(apiToken);
-			// int numOfDocuments = documents.length();
-			// System.out.println("Number of Documents = " + numOfDocuments);
-			// if ( numOfDocuments > 0 ) {
-			// 	for (int i = 0; i < numOfDocuments; i = i + 1) {
-			// 		JSONObject doc = documents.getJSONObject(i);
-			// 		System.out.println("Template:  " + doc.getInt("id") + ", " + doc.getString("name"));
-			// 		// System.out.println(doc.toString(2));
-			// 		// System.out.println();
-			// 	}
-			// }
-			// System.out.println();
+			JSONArray documents = capClient.showDocuments();
+			int numOfDocuments = documents.length();
+			System.out.println("Number of Documents = " + numOfDocuments);
+			if ( numOfDocuments > 0 ) {
+				for (int i = 0; i < numOfDocuments; i = i + 1) {
+					JSONObject doc = documents.getJSONObject(i);
+					System.out.println("Template:  " + doc.getInt("id") + ", " + doc.getString("name"));
+					// System.out.println(doc.toString(2));
+					// System.out.println();
+				}
+			}
+			System.out.println();
 			
       return;
     } catch (Throwable t) {
