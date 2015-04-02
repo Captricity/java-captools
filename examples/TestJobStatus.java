5f9d0c5afacd82e80568356b94c26ba76a6eac8d
Dave <daves@captricity.com>
2015-04-02 14:00:57 -0400

    working up to job results phase using TestJobStatus example

--- examples/TestJobStatus.java
+++ examples/TestJobStatus.java
@@ -12,14 +12,28 @@ public class TestJobStatus {
       JSONArray batches = capClient.showBatches();
 			int numOfBatches = batches.length();
 			System.out.println("Number of Batches = " + numOfBatches);
+			System.out.println();
+			
 			if ( numOfBatches > 0 ) {
 				for (int i = 0; i < numOfBatches; i = i + 1) {
-					JSONObject batch = batches.getJSONObject(i);
-					System.out.println("Batch:  " + batch.getInt("id") + ", " + 
-														  batch.getString("name") + " (files = " + batch.getInt("file_count") + ") - " +
-															batch.getString("status"));
+					JSONObject obj = batches.getJSONObject(i);
+					JSONObject batch = capClient.readBatch(obj.getInt("id"));
+					
+					System.out.println("Batch:  " + batch.getInt("id"));
+					System.out.println("Name: " + batch.getString("name"));
+					System.out.println("Status: " + batch.getString("status"));
+					System.out.println("Is digitized: " + batch.getBoolean("is_digitized"));
+					System.out.println("File Count: " + batch.getInt("file_count"));
+					if ( ! batch.isNull("related_job_id") ) {
+						System.out.println("Related Job ID: " + batch.getInt("related_job_id"));
+					}
+					// System.out.println(": " + batch.getString(""));
+					// System.out.println(": " + batch.getString(""));
+					// System.out.println(": " + batch.getString(""));
+					// System.out.println(": " + batch.getString(""));
+					System.out.println();
 					// System.out.println(batch.toString(2));
-					// System.out.println();
+					System.out.println();
 				}
 			}
 			System.out.println();
